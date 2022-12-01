3c5433205238a48e7b006cda5cc0a11ef7729904
Manfred Mjka <manixx90@gmail.com>
2022-12-01 20:04:00 +0100

    chore: small cleanup

--- config.h
+++ config.h
@@ -40,9 +40,7 @@ static const Rule rules[] = {
 	/* class instance            title tags mask     isfloating   monitor */
 	{  NULL, "chromium",         NULL, 1 << 1,       0,           -1 },
 	{  NULL, "web.whatsapp.com", NULL, 1 << 3,       0,           -1 },
-	{  NULL, "matterhorn",       NULL, 1 << 3,       0,           -1 },
 	{  NULL, "telegram-desktop", NULL, 1 << 3,       0,           -1 },
-	{  NULL, "st-float",         NULL, -1,           0,           -1 },
 };
 
 /* layout(s) */
