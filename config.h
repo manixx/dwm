abf23ef1d4c5e50c84da3cf8daa3647b21273608
Manfred Krieger <manfred.krieger@sclable.com>
2026-09-04 20:44:34 +0200

    chore: add 6th space

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ static const char *colors[][3]     = {
 };
 
 /* tagging */
-static const char *tags[] = { "•", "•", "•", "•", "•" };
+static const char *tags[] = { "•", "•", "•", "•", "•", "•" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -108,6 +108,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
 	{ MODKEY|ShiftMask,             XK_Escape, quit,           {0} },
 	{ MODKEY,                       XK_plus,  scratchpad_show, {0} },
 	{ MODKEY|ShiftMask,             XK_plus,  scratchpad_hide, {0} },
